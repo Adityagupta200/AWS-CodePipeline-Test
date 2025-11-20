12aa3a5f140435043e5ae331e78ca8848f927254
Vicky Gupta <aditya2004g@gmail.com>
2025-11-20 19:02:16 +0530

    Updated test_addition.py

--- tests/test_addition.py
+++ tests/test_addition.py
@@ -1,7 +1,7 @@
 from tests.my_module import add
 
-def test_add_positive_numbers(a, b):
+def test_add_positive_numbers():
 	assert add(2, 3) == 5
 
-def test_add_negative_numbers(a, b):
+def test_add_negative_numbers():
 	assert add(-2,- 3) == -5
